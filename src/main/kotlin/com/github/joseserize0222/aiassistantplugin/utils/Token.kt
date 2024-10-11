24d373693e6fd9a6aa922f17a4ab3542822f030b
joseserize0222 <joseserize0222@gmail.com>
2024-10-11 17:31:08 +0200

    Changing the Token env

--- src/main/kotlin/com/github/joseserize0222/aiassistantplugin/utils/Token.kt
+++ src/main/kotlin/com/github/joseserize0222/aiassistantplugin/utils/Token.kt
@@ -1,11 +1,13 @@
 package com.github.joseserize0222.aiassistantplugin.utils
 
+import com.intellij.util.EnvironmentUtil
+
 @JvmInline
 value class Token(val value: String)
 
 
 fun getToken() : Token{
-    val envToken = System.getenv("OPENAI_API_KEY")
+    val envToken = EnvironmentUtil.getValue("OPENAI_API_KEY")
     checkNotNull(envToken) {
         "No environment OPENAI_API_KEY was found"
     }
